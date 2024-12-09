a78f0584b56c48d2a21bfabea836440e54acb368
Nikoloz Garibashvili <nikagaribashvili@gmail.com>
2024-12-09 20:31:25 +0000

    config(editor): added keymaps for quickfix

--- init.lua
+++ init.lua
@@ -190,6 +190,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Keybinds to jump in the quickfix buffer
+vim.keymap.set('n', '[q', ':cnext<CR>zz', { desc = 'Jump to the next item in quickfix' })
+vim.keymap.set('n', ']q', ':cprevious<CR>zz', { desc = 'Jump to the previous item in quickfix' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
