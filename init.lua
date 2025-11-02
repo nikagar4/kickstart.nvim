bef74ffd41b988e1275b0e2f22c3641e900306b9
Nikoloz Garibashvili <12752783+nikagar4@users.noreply.github.com>
2025-11-02 12:41:28 +0400

    fixes indentation

--- init.lua
+++ init.lua
@@ -141,7 +141,7 @@ require 'custom.autocommands'
 --
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  { 'NMAC427/guess-indent.nvim', opts = {} }, -- Detect tabstop and shiftwidth automatically
 
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
